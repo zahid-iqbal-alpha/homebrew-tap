4ceccf1ef6eecf96f3ec72eeb99341e96370cee4
zahid-iqbal-alpha <zahid.iqbal@forenax.com>
2024-03-14 06:04:42 +0500

    updated formulae

--- Formula/grapple-cli.rb
+++ Formula/grapple-cli.rb
@@ -2,6 +2,6 @@ require "formula"
 require_relative "../custom_download_strategy.rb"
 class GrappleCli < Formula
   homepage "https://github.com/grapple-solutions/grapple-cli"
-  url "https://api.github.com/repos/grapple-solutions/grapple-cli/zipball/0.2.1", :using => GitHubPrivateRepositoryReleaseDownloadStrategy 
+  url "https://api.github.com/repos/grapple-solutions/grapple-cli/releases/146324191", :using => GitHubPrivateRepositoryReleaseDownloadStrategy 
   sha256 :no_check
 end
