1a8636e454b618a3808d5cb5945ec5d12b885bed
zahid-iqbal-alpha <163305499+zahid-iqbal-alpha@users.noreply.github.com>
2024-03-14 06:18:02 +0500

    Update grapple-cli.rb

--- Formula/grapple-cli.rb
+++ Formula/grapple-cli.rb
@@ -2,5 +2,5 @@ require "formula"
 class GrappleCli < Formula
   homepage "https://github.com/grapple-solutions/grapple-cli"
   url "https://api.github.com/repos/grapple-solutions/grapple-cli/releases/146324191" 
-  sha256 :no_check
+  sha256 "aa0b7bbcff326aa17d0b9991406b0a544e0df7a65314cfe43c26339ae9617b23"
 end
