402675ca3f3977ad8fab39394c43083ce75eb82b
zahid-iqbal-alpha <163305499+zahid-iqbal-alpha@users.noreply.github.com>
2024-03-14 06:16:46 +0500

    Update grapple-cli.rb

--- Formula/grapple-cli.rb
+++ Formula/grapple-cli.rb
@@ -1,18 +1,7 @@
-cask "grapple-cli" do
-
-  version :latest
-  sha256 :no_check
-
-  desc "grpl is a cli tool used to deploy and manage grapple on kubernetes cluster"
+require "formula"
+require_relative "../custom_download_strategy.rb"
+class GrappleCli < Formula
   homepage "https://github.com/grapple-solutions/grapple-cli"
-  url do
-   latest = GitHub.get_release("grapple-solutions", "grapple-cli", "0.2.1").fetch("zipball_url")
-   puts "GitHub::API : #{GitHub::API}"
-   [latest, header: [
-     "Accept: application/octet-stream",
-     "Authorization: bearer #{GitHub::API.credentials}"
-   ]]
-  end
-
-
+  url "https://api.github.com/repos/grapple-solutions/grapple-cli/releases/146324191" 
+  sha256 :no_check
 end
