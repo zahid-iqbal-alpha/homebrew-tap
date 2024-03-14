b11ce64ad465dc297ccddc9ec6651598fc9dadfd
zahid-iqbal-alpha <163305499+zahid-iqbal-alpha@users.noreply.github.com>
2024-03-14 06:47:43 +0500

    Update grapple-cli.rb

--- Formula/grapple-cli.rb
+++ Formula/grapple-cli.rb
@@ -4,6 +4,10 @@ class GrappleCli < Formula
   url "https://github.com/grapple-solutions/grapple-cli/archive/refs/tags/0.2.1.tar.gz"
   sha256 "f19d949c29a3bb2770c7ec4476076409d9e5c364b19bfc2043d385f93a2194bd"
 
+  def install
+    bin.install "grpl"
+  end
+  
   test do
     system "#{bin}/grpl", "--version"
   end
