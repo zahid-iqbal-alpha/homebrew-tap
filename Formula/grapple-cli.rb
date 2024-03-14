3127b4c432a9b8258edf10ce44ec441815e54db0
zahid-iqbal-alpha <163305499+zahid-iqbal-alpha@users.noreply.github.com>
2024-03-14 06:17:06 +0500

    Update grapple-cli.rb

--- Formula/grapple-cli.rb
+++ Formula/grapple-cli.rb
@@ -1,5 +1,4 @@
 require "formula"
-require_relative "../custom_download_strategy.rb"
 class GrappleCli < Formula
   homepage "https://github.com/grapple-solutions/grapple-cli"
   url "https://api.github.com/repos/grapple-solutions/grapple-cli/releases/146324191" 
