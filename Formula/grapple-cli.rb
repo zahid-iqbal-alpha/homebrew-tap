673316d8a12b813789880c59369e93a22c2ef261
zahid-iqbal-alpha <163305499+zahid-iqbal-alpha@users.noreply.github.com>
2024-03-14 14:17:22 +0500

    Update grapple-cli.rb

--- Formula/grapple-cli.rb
+++ Formula/grapple-cli.rb
@@ -1,8 +1,19 @@
 class GrappleCli < Formula
   desc "Command-line interface for Grapple"
   homepage "https://github.com/grapple-solutions/grapple-cli"
-  url "https://github.com/grapple-solutions/grapple-cli/archive/refs/tags/0.2.1.tar.gz"
-  sha256 "f19d949c29a3bb2770c7ec4476076409d9e5c364b19bfc2043d385f93a2194bd"
+
+  def latest_release_version(repo_owner, repo_name)
+    url = "https://api.github.com/repos/#{repo_owner}/#{repo_name}/releases/latest"
+    response = URI.open(url)
+    latest_release = JSON.parse(response.read)
+    latest_release["tag_name"]
+  end
+
+  version latest_release_version("grapple-solutions", "grapple-cli")
+
+  url "https://github.com/grapple-solutions/grapple-cli/archive/v#{version}.tar.gz"
+
+  sha256 Digest::SHA2.file("#{version}.tar.gz").hexdigest
 
   def install
     libexec.install Dir["*"]
