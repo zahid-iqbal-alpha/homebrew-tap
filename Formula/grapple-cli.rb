4de3555e236933954ad5d53104366064ebe4276f
zahid-iqbal-alpha <163305499+zahid-iqbal-alpha@users.noreply.github.com>
2024-03-14 06:38:43 +0500

    Update grapple-cli.rb

--- Formula/grapple-cli.rb
+++ Formula/grapple-cli.rb
@@ -2,7 +2,7 @@ class GrappleCli < Formula
   desc "Command-line interface for Grapple"
   homepage "https://github.com/grapple-solutions/grapple-cli"
   url "https://github.com/grapple-solutions/grapple-cli/archive/refs/tags/0.2.1.tar.gz"
-  sha256 "aa0b7bbcff326aa17d0b9991406b0a544e0df7a65314cfe43c26339ae9617b23"
+  sha256 "f19d949c29a3bb2770c7ec4476076409d9e5c364b19bfc2043d385f93a2194bd"
 
   test do
     system "#{bin}/grpl", "--version"
